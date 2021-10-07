52202cac0e206db51227f70aadb7fa53bcde7195
drasre <esquiveldrasly@outlook.es>
2021-10-06 23:28:55 -0600

    update subcateogiras module

--- sub-categorias.module.ts
+++ sub-categorias.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { subCategoriasEntity } from './subCategorias.entity';
+import { SubCategoriasController } from './sub-categorias.controller';
+import { SubCategoriasService} from './sub-categorias.service';
 
-@Module({})
+
+@Module({
+
+    imports: [TypeOrmModule.forFeature([subCategoriasEntity])],
+    providers: [SubCategoriasService],
+    controllers: [SubCategoriasController]
+
+})
 export class SubCategoriasModule {}
+
+
+
+
